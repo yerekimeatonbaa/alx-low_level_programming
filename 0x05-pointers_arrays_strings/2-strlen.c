833982810e7dfc0421d806099e85c2378499d4e5
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-09-19 19:18:54 -0700

    jj

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
-/**
- * _strlen - this function change the initial.
- * @s: this is a parameter to change.
- * Return: len
+/*
+ * File: 2-strlen.c
+ * Auth: Brennan D Baraban
  */
-int _strlen(char *s)
-{
-	int len;
 
-	while (*s != '\0')
-	{
-		len = len + 1;
-		s = s + 1;
-	}
-	return (len);
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
 }
