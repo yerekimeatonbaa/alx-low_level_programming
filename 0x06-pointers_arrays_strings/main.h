d639451d180890bcce97f14b280988732a68a27b
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-09-21 18:27:40 -0700

    ddf

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-0.	Int _putchar (char C)
-1.	char *_strcat(char *dest, char *src);
-2.	char *_strncat(char *dest, char *src, int n);
-3.	char *_strncpy(char *dest, char *src, int n);
-4.	int _strcmp(char *s1, char *s2);
-5.	void reverse_array(int *a, int n);
-6.	char *string_toupper(char *);
-7.	char *cap_string(char *);
-8.	char *leet(char *)
-
-endif
+#ifndef MAIN_H                                                                      
+#define MAIN_H                                                                      
+                                                                                    
+Int _putchar (char C)                                                       
+char *_strcat(char *dest, char *src);                                       
+char *_strncat(char *dest, char *src, int n);                               
+char *_strncpy(char *dest, char *src, int n);                               
+int _strcmp(char *s1, char *s2);                                            
+void reverse_array(int *a, int n);                                          
+char *string_toupper(char *);                                               
+char *cap_string(char *);                                                   
+char *leet(char *)                                                          
+                                                                                    
+endif 
