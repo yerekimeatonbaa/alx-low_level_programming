2f904b8edb2e5f8ed677094def63794ddf12553e
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-09-22 18:54:38 -0700

    dfdf

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
-*_strcat - concartenates 2 strings
-*@dets: destination array
-*@src: source array
-*Return: destination array
-*/
+ * _strcat - concatenates two strings
+ * @dest: dest string
+ * @src: src string
+ * Return: pointer to resulting string
+ */
 
 char *_strcat(char *dest, char *src)
 {
-char *ptr = dest;
-while (*ptr !=0)
-ptr++;
-while (*src !=0)
-*ptr++ = *src++;
-*ptr =0;
-return (dest);
+	int s = 0, i = 0;
 
+	while (dest[s] != 0)
+	{
+		s++;
+	}
+	while (src[i] != 0)
+	{
+		dest[s + i] = src[i];
+		i++;
+	}
+
+	dest[s + i] = 0;
+	return (dest);
 }
