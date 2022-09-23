ce11ee13f90ab68f79ee4ee52643c092c908ee18
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-09-22 17:13:54 -0700

    fgf

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,20 @@
-#include "main.h"                                                                   
-                                                                                                                                                                                                                                                        
-/**                                                                                 
- * strcat - Concatenates the string pointed to by @src, including the terminating   
- *          null byte, to the end of the string pointed to by @dest.                
- * @dest: A pointer to the string to be concatenated upon.                          
- * @src: The source string to be appended to @dest.                                 
- *                                                                                  
- * Return: A pointer to the destination string @dest.                               
- */                                                                                 
-char *strcat(char *dest, const char *src)                                           
-{                                                                                   
-        int index = 0, dest_len = 0;                                                
-                                                                                    
-        while (dest[index++])                                                       
-                dest_len++;                                                         
-                                                                                    
-        for (index = 0; src[index]; index++)                                        
-                dest[dest_len++] = src[index];                                      
-                                                                                    
-        return (dest);                                                              
-} 
+#include "main.h"
+
+/**
+*_strcat - concartenates 2 strings
+*@dets: destination array
+*@src: source array
+*Return: destination array
+*/
+
+char *_strcat(char *dest, char *src)
+{
+char *ptr = dest;
+while (*ptr !=0)
+ptr++;
+while (*src !=0)
+*ptr++ = *src++;
+*ptr =0;
+return (dest);
+
+}
