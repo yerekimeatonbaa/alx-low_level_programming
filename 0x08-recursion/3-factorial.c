f32ccb3353a1828c3fc22eb6ee55f6887d17b16f
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-09-29 05:20:04 -0700

    project commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - return the n factorial
+ * @n: number
+ * Return: factorial of n or -1 if negative
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
