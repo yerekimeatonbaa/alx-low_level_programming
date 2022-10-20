e1d742ec58491a9789e3eae4b515f023fc79eb22
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-10-20 16:13:05 -0700

    dd

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - function runs before main function
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI");
+	printf(" bore my house upon my back!\n");
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Stat
+
+
+
+
+
+
+
+
+
+
+
