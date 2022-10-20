90f766f9d5ec87da0fb467a9a9d18a5d07707f5a
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-10-19 19:50:51 -0700

    this is a commit

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,18 +1,15 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "oleh";
-    unsigned int n;
-
-    n = _strspn(s, f);
-    printf("%u\n", n);
-    return (0);
-}
+**                                                                                           
+ * main - check the code for ALX Afirca                                       
+ *                                                                                            
+ * Return: Always 0.                                                                          
+ */                                                                                           
+int main(void)                                                                                
+{                                                                                             
+    char *s = "hello, world";                                                                 
+    char *f = "oleh";                                                                         
+    unsigned int n;                                                                           
+                                                                                              
+    n = _strspn(s, f);                                                                        
+    printf("%u\n", n);                                                                        
+    return (0);                                                                               
+}  
