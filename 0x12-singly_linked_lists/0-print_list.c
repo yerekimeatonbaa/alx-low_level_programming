7660ce772d6e25a9e99b1c7f43fb3225a4034f33
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-10-20 17:05:46 -0700

    df

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,27 @@
-cat > 0-print_list.c
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_list - print all element of a linked list
- * @h: head of the linked list
- *
- * Return: number of nodes of the linked list
- */
+* print_list - print the elements of a linked list.
+* @h: liked list head.
+*
+* Return: amount of nodes of the linked list.
+*
+*/
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+size_t count = 0;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		count++;
-		h = h->next;
-	}
-	return (count);
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
 }
