b1879fd2935bba58e80836292818219a7c1f3254
Tonbra Yerekimea <yerekimeatonbaa@gmail.com>
2022-10-25 20:53:34 -0700

    mkj

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,31 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-/**
- * print_list - a function that prints all the elements of a list_t list.
- * @h: struct
- * Return: number of nodes
- */
-size_t print_list(const list_t *h)
-{
-	int count = 0;
 
-	if (h == NULL)
-		return (0);
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		nnodes++;
 	}
-
-	return (count);
+	return (nnodes);
 }
